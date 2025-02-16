7680e5441d0e574383d6d834834d7d65dea53da9
Pavel Palancica <ppalancica@Pavels-MacBook-Pro.local>
2025-02-17 00:00:51 +0200

    Added ViewLifecycleObserversSampleApp target

---
+++ ViewLifecycleObserversSampleApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ViewLifecycleObserversSampleApp
+//
+//  Created by Pavel Palancica  on 16.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
